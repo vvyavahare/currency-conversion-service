356e5d61cede8ab5036464063d1bb38e91156dd3
vvyavahare <vishal.vyavahare@clover.com>
2022-06-25 18:38:46 +0530

    Changes for sending data to kafka for conversion record auditing.

--- src/main/java/com/currency/microservices/currencyconversionservice/CurrencyConversion.java
+++ src/main/java/com/currency/microservices/currencyconversionservice/CurrencyConversion.java
@@ -1,89 +1,90 @@
 package com.currency.microservices.currencyconversionservice;
 
+import lombok.ToString;
+
 import java.math.BigDecimal;
 
+@ToString
 public class CurrencyConversion {
-	private Long id;
-	private String from;
-	private String to;
-	private BigDecimal quantity;
-	private BigDecimal conversionMultiple;
-	private BigDecimal totalCalculatedAmount;
-	private String environment;
+  private Long id;
+  private String from;
+  private String to;
+  private BigDecimal quantity;
+  private BigDecimal conversionMultiple;
+  private BigDecimal totalCalculatedAmount;
+  private String environment;
 
-	public CurrencyConversion() {
-		
-	}
-	
-	public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, 
-			BigDecimal totalCalculatedAmount, String environment) {
-		super();
-		this.id = id;
-		this.from = from;
-		this.to = to;
-		this.conversionMultiple = conversionMultiple;
-		this.quantity = quantity;
-		this.totalCalculatedAmount = totalCalculatedAmount;
-		this.environment = environment;
-	}
+  public CurrencyConversion() {
 
-	public Long getId() {
-		return id;
-	}
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple,
+                            BigDecimal totalCalculatedAmount, String environment) {
+    super();
+    this.id = id;
+    this.from = from;
+    this.to = to;
+    this.conversionMultiple = conversionMultiple;
+    this.quantity = quantity;
+    this.totalCalculatedAmount = totalCalculatedAmount;
+    this.environment = environment;
+  }
 
-	public String getFrom() {
-		return from;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setFrom(String from) {
-		this.from = from;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getTo() {
-		return to;
-	}
+  public String getFrom() {
+    return from;
+  }
 
-	public void setTo(String to) {
-		this.to = to;
-	}
+  public void setFrom(String from) {
+    this.from = from;
+  }
 
-	public BigDecimal getConversionMultiple() {
-		return conversionMultiple;
-	}
+  public String getTo() {
+    return to;
+  }
 
-	public void setConversionMultiple(BigDecimal conversionMultiple) {
-		this.conversionMultiple = conversionMultiple;
-	}
+  public void setTo(String to) {
+    this.to = to;
+  }
 
-	public BigDecimal getQuantity() {
-		return quantity;
-	}
+  public BigDecimal getConversionMultiple() {
+    return conversionMultiple;
+  }
 
-	public void setQuantity(BigDecimal quantity) {
-		this.quantity = quantity;
-	}
+  public void setConversionMultiple(BigDecimal conversionMultiple) {
+    this.conversionMultiple = conversionMultiple;
+  }
 
-	public BigDecimal getTotalCalculatedAmount() {
-		return totalCalculatedAmount;
-	}
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
 
-	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
-		this.totalCalculatedAmount = totalCalculatedAmount;
-	}
+  public void setQuantity(BigDecimal quantity) {
+    this.quantity = quantity;
+  }
 
-	public String getEnvironment() {
-		return environment;
-	}
+  public BigDecimal getTotalCalculatedAmount() {
+    return totalCalculatedAmount;
+  }
 
-	public void setEnvironment(String environment) {
-		this.environment = environment;
-	}
-	
-	
-	
+  public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+    this.totalCalculatedAmount = totalCalculatedAmount;
+  }
+
+  public String getEnvironment() {
+    return environment;
+  }
+
+  public void setEnvironment(String environment) {
+    this.environment = environment;
+  }
+
 
 }
--- src/main/java/com/currency/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/currency/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,60 +3,81 @@ package com.currency.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Slf4j
 public class CurrencyConversionController {
-	
-	@Autowired
-	private CurrencyExchangeProxy proxy;
-	
-	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion calculateCurrencyConversion(
-			@PathVariable String from,
-			@PathVariable String to,
-			@PathVariable BigDecimal quantity
-			) {
-		
-		HashMap<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("from",from);
-		uriVariables.put("to",to);
-		
-		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
-		("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
-				CurrencyConversion.class, uriVariables);
-		
-		CurrencyConversion currencyConversion = responseEntity.getBody();
-		
-		return new CurrencyConversion(currencyConversion.getId(), 
-				from, to, quantity, 
-				currencyConversion.getConversionMultiple(), 
-				quantity.multiply(currencyConversion.getConversionMultiple()), 
-				currencyConversion.getEnvironment()+ " " + "rest template");
-		
-	}
 
-	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion calculateCurrencyConversionFeign(
-			@PathVariable String from,
-			@PathVariable String to,
-			@PathVariable BigDecimal quantity
-			) {
-				
-		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
-		
-		return new CurrencyConversion(currencyConversion.getId(), 
-				from, to, quantity, 
-				currencyConversion.getConversionMultiple(), 
-				quantity.multiply(currencyConversion.getConversionMultiple()), 
-				currencyConversion.getEnvironment() + " " + "feign");
-		
-	}
+  @Value(value = "${kafka.topic.currency.conversion}")
+  private String currencyConversionTopic;
+
+  @Autowired
+  private CurrencyExchangeProxy proxy;
+
+  @Autowired
+  private KafkaTemplate kafkaTemplate;
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+  public CurrencyConversion calculateCurrencyConversion(
+      @PathVariable String from,
+      @PathVariable String to,
+      @PathVariable BigDecimal quantity
+  ) {
+
+    HashMap<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
+
+    ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
+        ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+         CurrencyConversion.class, uriVariables);
+
+    CurrencyConversion currencyConversion = responseEntity.getBody();
+
+    return new CurrencyConversion(currencyConversion.getId(),
+                                  from, to, quantity,
+                                  currencyConversion.getConversionMultiple(),
+                                  quantity.multiply(currencyConversion.getConversionMultiple()),
+                                  currencyConversion.getEnvironment() + " " + "rest template");
+
+  }
+
+  @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+  public CurrencyConversion calculateCurrencyConversionFeign(
+      @PathVariable String from,
+      @PathVariable String to,
+      @PathVariable BigDecimal quantity
+  ) {
+
+    CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+    try {
+      kafkaTemplate.send(currencyConversionTopic, mapper.writeValueAsString(currencyConversion));
+    } catch (Exception e) {
+      log.error("Error in sending data to kafka.", e);
+    }
+
+    return new CurrencyConversion(currencyConversion.getId(),
+                                  from, to, quantity,
+                                  currencyConversion.getConversionMultiple(),
+                                  quantity.multiply(currencyConversion.getConversionMultiple()),
+                                  currencyConversion.getEnvironment() + " " + "feign");
+
+  }
 
 
 }
